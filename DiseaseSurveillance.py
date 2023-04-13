1c3c09ca0ebfe23b27b53d2a258105183fe3b7a3
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-04-13 23:53:02 +0800

    Update DiseaseSurveillance.py

--- DiseaseSurveillance.py
+++ DiseaseSurveillance.py
@@ -1,69 +1,155 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+from fpdf import FPDF
 
-# Set page title and favicon
-st.set_page_config(page_title="Disease Surveillance System", page_icon=":microbe:")
+# Define your knowledge base as a dictionary
+knowledge_base = {
+    "question_1": {
+        "text": "How often have you been feeling down, depressed, or hopeless in the past 2 weeks?",
+        "options": {
+            "Not at all": "question_2",
+            "Several days": "question_3",
+            "More than half the days": "question_4",
+            "Nearly every day": "question_5"
+        }
+    },
+    "question_2": {
+        "text": "Have you experienced a loss of interest or pleasure in doing things that you normally enjoy?",
+        "options": {
+            "No": "result_no_depression",
+            "Yes": "question_6"
+        }
+    },
+    "question_3": {
+        "text": "Have you experienced a loss of interest or pleasure in doing things that you normally enjoy?",
+        "options": {
+            "No": "result_mild_depression",
+            "Yes": "question_6"
+        }
+    },
+    "question_4": {
+        "text": "Have you experienced a loss of interest or pleasure in doing things that you normally enjoy?",
+        "options": {
+            "No": "result_mild_depression",
+            "Yes": "question_6"
+        }
+    },
+    "question_5": {
+        "text": "Have you experienced a loss of interest or pleasure in doing things that you normally enjoy?",
+        "options": {
+            "No": "result_severe_depression",
+            "Yes": "question_6"
+        }
+    },
+    "question_6": {
+        "text": "Have you experienced a significant weight loss or gain?",
+        "options": {
+            "No": "question_7",
+            "Yes": "result_severe_depression"
+        }
+    },
+    "question_7": {
+        "text": "Have you experienced trouble sleeping or sleeping too much?",
+        "options": {
+            "No": "question_8",
+            "Yes": "result_mild_depression"
+        }
+    },
+    "question_8": {
+        "text": "Have you experienced feelings of worthlessness or excessive guilt?",
+        "options": {
+            "No": "question_9",
+            "Yes": "result_mild_depression"
+        }
+    },
+    "question_9": {
+        "text": "Have you experienced difficulty in concentrating or making decisions?",
+        "options": {
+            "No": "result_mild_depression",
+            "Yes": "result_severe_depression"
+        }
+    },
+    "result_no_depression": {
+        "text": "Based on your responses, it appears that you are not experiencing depression.",
+        "recommendation": "Keep up the good work and stay healthy! If you feel your mood changing, check in with your healthcare provider."
+    },
+    "result_mild_depression": {
+        "text": "Based on your responses, it appears that you may be experiencing mild depression.",
+        "recommendation": "It's important to take care of your mental health. Try engaging in activities that you enjoy, getting regular exercise, and reaching out to a mental health professional for support."
+    },
+    "result_severe_depression": {
+        "text": "Based on your responses, it appears that you may be experiencing severe depression.",
+        "recommendation": "It's important to seek professional help as soon as possible. Reach out to your healthcare provider or a mental health professional for support and guidance. They can help you create a treatment plan and provide you with the necessary resources to support your mental health."
+    }
+}
 
-# Set color palette
-primary_color = "#472731"
-secondary_color = "#282130"
-accent_color = "#F18A85"
-background_color = "#849DAB"
-text_color = "#24788F"
+#Define a function to generate the PDF report
+def generate_pdf_report(result):
+    # Create a PDF object
+    pdf = FPDF()
+    # Add a page
+    pdf.add_page()
+    # Set the font
+    pdf.set_font("Arial", size=12)
+    # Add the text
+    pdf.cell(200, 10, txt="Depression Detection Results", ln=1, align="C")
+    pdf.cell(200, 10, txt=" ", ln=1)
+    pdf.cell(200, 10, txt=result["text"], ln=1)
+    pdf.cell(200, 10, txt=" ", ln=1)
+    pdf.cell(200, 10, txt="Recommendation:", ln=1)
+    pdf.cell(200, 10, txt=result["recommendation"], ln=1)
+    # Save the PDF
+    pdf.output("Depression_Detection_Results.pdf")
+    
+#Define the Streamlit app
+def app():
+    # Set the app title and page icon
+    st.set_page_config(page_title="Early Depression Detection", page_icon=":smiley:")
+    # Set the app header
+    st.header("Early Depression Detection")
+    # Set the app subheader
+    st.subheader("Answer the following questions to detect depression early")
+    # Set the app background color
+    st.markdown(
+        """
+        <style>
+            body {
+                background-color: #849DAB;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    # Initialize the answer dictionary
+    answers = {}
+    # Loop through the knowledge base
+    current_question = "question_1"
+    while current_question is not None:
+        # Display the question
+        question = knowledge_base[current_question]["text"]
+        options = list(knowledge_base[current_question]["options"].keys())
+        answer = st.radio(question, options)
+        # Update the answer dictionary
+        answers[current_question] = answer
+        # Move to the next question
+        current_question = knowledge_base[current_question]["options"].get(answer)
+    # Get the result
+    result = knowledge_base[current_question]
+    # Display the result
+    st.markdown(
+        f"""
+        <div style='background-color: #F18A85; padding: 10px'>
+            <h3 style='color: white'>{result["text"]}</h3>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    st.write(result["recommendation"])
+    # Add a button to generate the PDF report
+    if st.button("Generate PDF report"):
+        generate_pdf_report(result)
+        st.success("PDF report generated successfully!")
 
-# Set page layout
-st.markdown(f"""
-    <style>
-        .reportview-container {{
-            background-color: {background_color};
-            color: {text_color};
-        }}
-        .sidebar .sidebar-content {{
-            background-color: {primary_color};
-        }}
-        .btn-primary {{
-            background-color: {accent_color};
-            color: {text_color};
-            font-weight: bold;
-        }}
-    </style>
-""", unsafe_allow_html=True)
-
-# Create header section
-st.markdown(f"<h1 style='text-align: center; color: {accent_color};'>Disease Surveillance System</h1>", unsafe_allow_html=True)
-st.write("This system monitors disease outbreaks and provides early warning to public health officials to help prevent the spread of infectious diseases.")
-
-# Create data input section
-st.subheader("Data Input")
-data_file = st.file_uploader("Upload data file", type=["csv", "xlsx"])
-
-if data_file is not None:
-    data = pd.read_csv(data_file)
-
-    # Create data summary section
-    st.subheader("Data Summary")
-    st.write("Here is a summary of the data:")
-    st.write(data.describe())
-
-    # Create data visualization section
-    st.subheader("Data Visualization")
-    chart_type = st.selectbox("Select chart type", ["Line Chart", "Bar Chart"])
-    x_column = st.selectbox("Select x-axis column", data.columns)
-    y_column = st.selectbox("Select y-axis column", data.columns)
-
-    if chart_type == "Line Chart":
-        st.line_chart(data[[x_column, y_column]])
-    else:
-        st.bar_chart(data[[x_column, y_column]])
-
-    # Create analysis section
-    st.subheader("Analysis")
-    threshold = st.number_input("Enter outbreak threshold", value=10)
-    outbreak_data = data[data[y_column] >= threshold]
-
-    if len(outbreak_data) > 0:
-        st.write("Outbreak detected!")
-        st.write(outbreak_data)
-    else:
-        st.write("No outbreak detected.")
+#Run the app
+if name == "main":
+    app()
